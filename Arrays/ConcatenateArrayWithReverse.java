b55ccb6c804fb65cce86479f1114b71d9405cca5
krishnrajchauhan <krishnrajsinh1911@gmail.com>
2026-08-20 09:39:06 +0530

    solved

---
+++ Arrays/ConcatenateArrayWithReverse.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+        }
+
+        for(int i = 0; i < n; i++) {
+            ans[i + n] = nums[n - 1 - i];
+        }
+
+        return ans;
+    }
+}
