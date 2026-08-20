988f16fe101a50c575fbad9b1a9959c68c37f2e8
krishnrajchauhan <krishnrajsinh1911@gmail.com>
2026-08-20 09:40:19 +0530

    Solved

--- Strings/FIndTheDifference.java
+++ Strings/FIndTheDifference.java
@@ -2,31 +2,19 @@ class Solution {
     public char findTheDifference(String s, String t) {
         int[] count = new int[26];
 
-        if(s.length() == 0 && t.length() == 0) {
-            return "";
-        }
-        else if(s.length() == 0 && t.length() != 0) {
-            return t;
-        }
-        else if(s.length() != 0 && t.length() == 0) {
-            return s;
-        }
-        else {
-            for(char c : s.toCharAyyay()) {
-                count[c - 'a']++;
-            }
-
-            for(char c : t.toCharArray()) {
-                count[c - 'a']++;
-            }
-
-            for(int i = 0; i < count.length; i++) {
-                if(count[i] == 1) {
-                    return count[i];
-                }
-            }
-        return -1;
+        for(char c : s.toCharArray()) {
+            count[c - 'a']--;
         }
 
+        for(char c : t.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] == 1){
+                return (char) (i + 'a');
+            }
+        }
+        return ' ';
     }
 }
